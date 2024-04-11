c14f850e020fefde723aead2b900e5b1e008ba09
HollowDancer <HollowDancer32@gmail.com>
2024-04-11 06:16:53 -0300

    second commit

--- Js/Main.js
+++ Js/Main.js
@@ -1,31 +1,103 @@
-    function jugarAdivinarNúmero() {
+const productos = [
+  {
+    nombre: "harina",
+    precio: 50,
+  },
+  {
+    nombre: "leche",
+    precio: 100,
+  },
+  {
+    nombre: "gaseosa",
+    precio: 150,
+  },
+  {
+    nombre: "pollo",
+    precio: 400,
+  },
+  {
+    nombre: "carne",
+    precio: 500,
+  },
+],
+carrito = [];
 
-    const numeroAleatorio= Math.floor(Math.random() * 100) +1;
-    console.log(numeroAleatorio)
+let seleccion = prompt("hola desea comprar algo? (si-no)");
 
-    let intentosTotales = 0;
-    let adivinado = false;
-
-    while (!adivinado){
-        let intento = parseInt(prompt ("Intentá adivinar un número entre 1 y 100. Tienes 5 intentos:"));
-        intentosTotales++;
-        
-        if (intento === numeroAleatorio) {
-            alert("Felicidades! El número aleatorio era " +numeroAleatorio +". Lo adivinaste en " +
-            intentosTotales + "Intentos.");
-            adivinado = true;
-            jugarAdivinarNúmero();
-        } else if (intento < numeroAleatorio) {
-            alert("El número es más alto. Intente nuevamente.");
-        } else  {
-            alert("El número es más bajo. Intente nuevamente.");
-    }
-    if (intentosTotales === 5) {
-        adivinado= true;
-        alert("¡Perdiste! Llegaste al maximo de intentos, vuelve a empezar.");
-        jugarAdivinarNúmero();
-    }
-    }
-    //Number respeta el número en su totalidad, en cambio parseint lo redondea
+while (seleccion != "si" && seleccion != "no") {
+alert("por favor ingrese una de las opciones, si o no");
+seleccion = prompt("Hola, desea comprar algo? (si-no) ");
 }
-jugarAdivinarNúmero();
+
+if (seleccion == "si") {
+alert("Disponemos de los siguientes productos");
+let todosLosProductos = productos.map(
+  (producto) => producto.nombre + " " + producto.precio + "$"
+);
+alert(todosLosProductos.join(" - "));
+} else if (seleccion == "no") {
+alert("gracias por comprar!");
+}
+
+while (seleccion != "no") {
+let producto = prompt("agregue un producto a su carrito");
+let precio = 0;
+
+if (
+  producto == "harina" ||
+  producto == "leche" ||
+  producto == "gaseosa" ||
+  producto == "pollo" ||
+  producto == "carne"
+
+) 
+{
+    switch (producto) {
+      case "harina":
+        precio = 50;
+        break;
+      case "leche":
+        precio = 100;
+        break;
+      case "gaseosa":
+        precio = 150;
+        break;
+      case "pollo":
+        precio = 400;
+        break;
+      case "carne":
+        precio = 500;
+        break;
+      default:
+        break;
+    }
+  let unidades = parseInt(
+    prompt("Cuantas unidades desea llevar?")
+  );
+
+  carrito.push({ producto, unidades, precio });
+  console.log(carrito);
+
+} else {
+  alert("no tenemos ese prodcto");
+}
+
+seleccion = prompt("quiere seguir comprando? (si-no)");
+
+while (seleccion == "no") {
+  alert("gracias por la compra!");
+
+  carrito.forEach((carritoFinal) => {
+    console.log(
+      `"producto:" ${carritoFinal.producto}, "unidades:" ${
+        carritoFinal.unidades
+      }, "total a pagar por producto:" ${carritoFinal.unidades * carritoFinal.precio}`
+    );
+  });
+  break;
+}
+}
+
+//al final con el metodo reduce se calcula el total de todos los productos con sus unidades!
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(`el total a pagar por su compra es de:  ${total}`)
