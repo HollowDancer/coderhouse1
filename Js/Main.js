aa19a8d2445e63cb08b9bb3edd502a47ebcd12b6
HollowDancer <HollowDancer32@gmail.com>
2024-04-29 18:05:23 -0300

    third commit
    tercer commit

--- Js/Main.js
+++ Js/Main.js
@@ -1,103 +1,201 @@
 const productos = [
   {
-    nombre: "harina",
+    id: 1,
+    nombre: "Huevos",
+    precio: 25,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpPyB7IJKK_cPNEfEpRz4x9bPJh8hM9ZbRVg3lpNIS7Q&s",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Harina",
     precio: 50,
+    img:
+      "https://images.ecestaticos.com/WBp24vmd9x9S722ZiqGWgPaf0w0=/0x109:2119x1301/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F6be%2F87b%2F448%2F6be87b448c2cf27ea55a994c163556d6.jpg",
+    cantidad: 1,
   },
   {
-    nombre: "leche",
+    id: 3,
+    nombre: "Leche",
     precio: 100,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIFTfoNPTrayDyzHMHPuPQGlPhKofQPPWw40zHNyodsA&s",
+    cantidad: 1,
   },
   {
+    id: 4,
     nombre: "gaseosa",
     precio: 150,
+    img:
+      "https://mayoristaelgringo.com.ar/wp-content/uploads/2023/01/7798113300010-17.jpg",
+    cantidad: 1,
   },
   {
-    nombre: "pollo",
+    id: 5,
+    nombre: "Pollo",
     precio: 400,
-  },
+    img:
+      "https://masonlineprod.vtexassets.com/arquivos/ids/166612/Pollo-Fresco-X-3-Kg-1-17088.jpg?v=637835138771500000",
+    cantidad: 1,
+  }, 
   {
-    nombre: "carne",
+    id: 6,
+    nombre: "Carne",
     precio: 500,
+    img:
+      "https://www.cocinista.es/download/bancorecursos/recetas/cocinar-la-carne-de-vacuno.jpg",
+    cantidad: 1,
   },
-],
-carrito = [];
+];
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const showAlert = document.getElementById("showAlert");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 
-let seleccion = prompt("hola desea comprar algo? (si-no)");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-while (seleccion != "si" && seleccion != "no") {
-alert("por favor ingrese una de las opciones, si o no");
-seleccion = prompt("Hola, desea comprar algo? (si-no) ");
-}
+productos.forEach((product) => {
+  let content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">${product.precio} $</p>
+  `;
 
-if (seleccion == "si") {
-alert("Disponemos de los siguientes productos");
-let todosLosProductos = productos.map(
-  (producto) => producto.nombre + " " + producto.precio + "$"
-);
-alert(todosLosProductos.join(" - "));
-} else if (seleccion == "no") {
-alert("gracias por comprar!");
-}
+  shopContent.append(content);
 
-while (seleccion != "no") {
-let producto = prompt("agregue un producto a su carrito");
-let precio = 0;
+  let comprar = document.createElement("button");
+  comprar.innerText = "comprar";
+  comprar.className = "comprar";
 
-if (
-  producto == "harina" ||
-  producto == "leche" ||
-  producto == "gaseosa" ||
-  producto == "pollo" ||
-  producto == "carne"
+  content.append(comprar);
 
-) 
-{
-    switch (producto) {
-      case "harina":
-        precio = 50;
-        break;
-      case "leche":
-        precio = 100;
-        break;
-      case "gaseosa":
-        precio = 150;
-        break;
-      case "pollo":
-        precio = 400;
-        break;
-      case "carne":
-        precio = 500;
-        break;
-      default:
-        break;
+  comprar.addEventListener("click", () => {
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+
+    if (repeat) {
+      carrito.map((prod) => {
+        if (prod.id === product.id) {
+          prod.cantidad++;
+        }
+      });
+    } else {
+      carrito.push({
+        id: product.id,
+        img: product.img,
+        nombre: product.nombre,
+        precio: product.precio,
+        cantidad: product.cantidad,
+      });
+      console.log(carrito);
+      console.log(carrito.length);
+      carritoCounter();
+      saveLocal();
     }
-  let unidades = parseInt(
-    prompt("Cuantas unidades desea llevar?")
-  );
-
-  carrito.push({ producto, unidades, precio });
-  console.log(carrito);
-
-} else {
-  alert("no tenemos ese prodcto");
-}
-
-seleccion = prompt("quiere seguir comprando? (si-no)");
-
-while (seleccion == "no") {
-  alert("gracias por la compra!");
-
-  carrito.forEach((carritoFinal) => {
-    console.log(
-      `"producto:" ${carritoFinal.producto}, "unidades:" ${
-        carritoFinal.unidades
-      }, "total a pagar por producto:" ${carritoFinal.unidades * carritoFinal.precio}`
-    );
   });
-  break;
-}
-}
+});
 
-//al final con el metodo reduce se calcula el total de todos los productos con sus unidades!
-const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
-console.log(`el total a pagar por su compra es de:  ${total}`)
+// Elegir producto
+const saveLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const pintarCarrito = () => {
+  modalContainer.innerHTML = "";
+  modalContainer.style.display = "flex";
+  const modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+  modalHeader.innerHTML = `
+      <h1 class="modal-header-title">Carrito.</h1>
+    `;
+  modalContainer.append(modalHeader);
+
+  const modalbutton = document.createElement("h1");
+  modalbutton.innerText = "x";
+  modalbutton.className = "modal-header-button";
+
+  modalbutton.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+  });
+
+  modalHeader.append(modalbutton);
+
+  carrito.forEach((product) => {
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "modal-content";
+    carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+        <span class="restar"> - </span>
+        <!--recomiendo no escribir la palabra cantidad para que no quede tan largo :)-->
+        <p>${product.cantidad}</p>
+        <span class="sumar"> + </span>
+        <p>Total: ${product.cantidad * product.precio} $</p>
+        <span class="delete-product"> ❌ </span>
+      `;
+
+    modalContainer.append(carritoContent);
+
+    let restar = carritoContent.querySelector(".restar");
+
+    restar.addEventListener("click", () => {
+      if (product.cantidad !== 1) {
+        product.cantidad--;
+      }
+      saveLocal();
+      pintarCarrito();
+    });
+
+    let sumar = carritoContent.querySelector(".sumar");
+    sumar.addEventListener("click", () => {
+      product.cantidad++;
+      saveLocal();
+      pintarCarrito();
+    });
+
+    let eliminar = carritoContent.querySelector(".delete-product");
+
+    eliminar.addEventListener("click", () => {
+      eliminarProducto(product.id);
+    });
+  });
+
+  const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+
+  const totalBuying = document.createElement("div");
+  totalBuying.className = "total-content";
+  totalBuying.innerHTML = `Total a pagar: ${total} $`;
+  modalContainer.append(totalBuying);
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+
+const eliminarProducto = (id) => {
+  const foundId = carrito.find((element) => element.id === id);
+
+  console.log(foundId);
+
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== foundId;
+  });
+
+  carritoCounter();
+  saveLocal();
+  pintarCarrito();
+};
+
+const carritoCounter = () => {
+  cantidadCarrito.style.display = "block";
+
+  const carritoLength = carrito.length;
+
+  localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+  cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
+
+carritoCounter();
