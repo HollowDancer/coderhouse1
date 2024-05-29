61654768697a11b510171ebbdd673ade47965577
HollowDancer <HollowDancer32@gmail.com>
2024-05-29 20:55:50 -0300

    Last commit

--- Js/Main.js
+++ Js/Main.js
@@ -1,53 +1,3 @@
-const productos = [
-  {
-    id: 1,
-    nombre: "Huevos",
-    precio: 25,
-    img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpPyB7IJKK_cPNEfEpRz4x9bPJh8hM9ZbRVg3lpNIS7Q&s",
-    cantidad: 1,
-  },
-  {
-    id: 2,
-    nombre: "Harina",
-    precio: 50,
-    img:
-      "https://images.ecestaticos.com/WBp24vmd9x9S722ZiqGWgPaf0w0=/0x109:2119x1301/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F6be%2F87b%2F448%2F6be87b448c2cf27ea55a994c163556d6.jpg",
-    cantidad: 1,
-  },
-  {
-    id: 3,
-    nombre: "Leche",
-    precio: 100,
-    img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIFTfoNPTrayDyzHMHPuPQGlPhKofQPPWw40zHNyodsA&s",
-    cantidad: 1,
-  },
-  {
-    id: 4,
-    nombre: "gaseosa",
-    precio: 150,
-    img:
-      "https://mayoristaelgringo.com.ar/wp-content/uploads/2023/01/7798113300010-17.jpg",
-    cantidad: 1,
-  },
-  {
-    id: 5,
-    nombre: "Pollo",
-    precio: 400,
-    img:
-      "https://masonlineprod.vtexassets.com/arquivos/ids/166612/Pollo-Fresco-X-3-Kg-1-17088.jpg?v=637835138771500000",
-    cantidad: 1,
-  }, 
-  {
-    id: 6,
-    nombre: "Carne",
-    precio: 500,
-    img:
-      "https://www.cocinista.es/download/bancorecursos/recetas/cocinar-la-carne-de-vacuno.jpg",
-    cantidad: 1,
-  },
-];
 const shopContent = document.getElementById("shopContent");
 const verCarrito = document.getElementById("verCarrito");
 const modalContainer = document.getElementById("modal-container");
@@ -56,47 +6,56 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  content.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3>
-    <p class="price">${product.precio} $</p>
-  `;
+const getProducts = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
+  console.log(data); // Para asegurarte de que los datos se están cargando correctamente
+  
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3>
+      <p class="price">${product.precio} $</p>
+    `;
 
-  shopContent.append(content);
+    shopContent.append(content);
 
-  let comprar = document.createElement("button");
-  comprar.innerText = "comprar";
-  comprar.className = "comprar";
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
 
-  content.append(comprar);
+    content.append(comprar);
 
-  comprar.addEventListener("click", () => {
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+    comprar.addEventListener("click", () => {
+      const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
 
-    if (repeat) {
-      carrito.map((prod) => {
-        if (prod.id === product.id) {
-          prod.cantidad++;
-        }
-      });
-    } else {
-      carrito.push({
-        id: product.id,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-      });
+      if (repeat) {
+        carrito = carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+          return prod;
+        });
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          nombre: product.nombre,
+          precio: product.precio,
+          cantidad: 1, // Inicializa la cantidad a 1
+        });
+      }
       console.log(carrito);
       console.log(carrito.length);
       carritoCounter();
       saveLocal();
-    }
+    });
   });
-});
+};
+
+getProducts();
 
 // Elegir producto
 const saveLocal = () => {
@@ -131,7 +90,6 @@ const pintarCarrito = () => {
         <h3>${product.nombre}</h3>
         <p>${product.precio} $</p>
         <span class="restar"> - </span>
-        <!--recomiendo no escribir la palabra cantidad para que no quede tan largo :)-->
         <p>${product.cantidad}</p>
         <span class="sumar"> + </span>
         <p>Total: ${product.cantidad * product.precio} $</p>
@@ -175,13 +133,7 @@ const pintarCarrito = () => {
 verCarrito.addEventListener("click", pintarCarrito);
 
 const eliminarProducto = (id) => {
-  const foundId = carrito.find((element) => element.id === id);
-
-  console.log(foundId);
-
-  carrito = carrito.filter((carritoId) => {
-    return carritoId !== foundId;
-  });
+  carrito = carrito.filter((element) => element.id !== id);
 
   carritoCounter();
   saveLocal();
